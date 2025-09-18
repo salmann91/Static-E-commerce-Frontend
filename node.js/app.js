fdc4143ea4a8948d537fe93cfb5a23e21f2b108d
salmann91 <ansarisalmanmd091@gmail.com>
2025-09-18 21:25:30 +0530

    Delete node.js directory

--- node.js/app.js
+++
@@ -1,6 +0,0 @@
-let http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World welcome in my world this is first code!');
-}).listen(8080);
